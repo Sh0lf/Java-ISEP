be3129ba31a615c214e8a64390ef8fa09843a4ae
YAP Vincent <80401462+Sh0lf@users.noreply.github.com>
2023-02-17 11:08:47 +0100

    Update Main.java

--- TP2/Main.java
+++ TP2/Main.java
@@ -1,4 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,7 +9,71 @@ public class Main {
         //parite();
         //max();
         //min();
-        egaliteStr();
+        //egaliteStr();
+        //factorielle();
+        //countdown();
+        //carres();
+        //tableMultiplication();
+        //division();
+        //regle();
+        //nombrePremier();
+        //initialisationTableau();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Quel exercice ? Saisissez le numéro:\n" +
+                "1. Discriminant\n" +
+                "2. Parité d’un nombre\n" +
+                "3. Calcul d’extremum\n" +
+                "4. Égalité entre chaînes de caractères\n" +
+                "5. Factorielle\n" +
+                "6. Compte à rebous\n" +
+                "7. Valeurs de carrés\n" +
+                "8. Table de multiplication\n" +
+                "9. Division d’entiers\n" +
+                "10. Règle graduée\n" +
+                "11. Nombres premiers\n" +
+                "12. Manipulations sur un tableau");
+        int res = scanner.nextInt();
+
+        switch(res)
+        {
+            case 1:
+                discriminant();
+                break;
+            case 2:
+                parite();
+                break;
+            case 3:
+                max();
+                break;
+            case 4:
+                egaliteStr();
+                break;
+            case 5:
+                factorielle();
+                break;
+            case 6:
+                countdown();
+                break;
+            case 7:
+                carres();
+                break;
+            case 8:
+                tableMultiplication();
+                break;
+            case 9:
+                division();
+                break;
+            case 10:
+                regle();
+                break;
+            case 11:
+                nombrePremier();
+                break;
+            case 12:
+                initialisationTableau();
+                break;
+        }
     }
     public static void discriminant() {
         Scanner scanner = new Scanner(System.in);
@@ -98,14 +165,152 @@ public class Main {
 
     public static void factorielle() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Saisir un entier positif ou nul");
-        int n = scanner.nextInt();
+        int entier;
+        do {
+            System.out.println("Veuillez saisir un entier positif ou nul");
+            entier = scanner.nextInt();
+        } while (entier < 0);
         int factorielle = 1;
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i <= entier; i++) {
             factorielle *= i;
         }
-        System.out.println(n + "! = " + factorielle);
+        System.out.println(entier + "! = " + factorielle);
+    }
+
+    public static void countdown(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Saisir un nombre");
+        int n = scanner.nextInt();
+        for (int i = n; i >= 0; i--) {
+            if (i==0) {
+                System.out.println("BOOM");
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+
+    public static void carres(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Saisir un nombre");
+        int n = scanner.nextInt();
+        int nSquared = n*n;
+        System.out.println(n + "  " + nSquared);
+    }
+
+    public static void tableMultiplication() {
+        for (int i = 1; i <= 10; i++){
+            for (int j = 1; j <=10; j++){
+                System.out.print(j*i + "  ");
+            }
+            System.out.print("\n");
+        }
+
+    }
+
+    public static void division() {
+        Scanner scanner = new Scanner(System.in);
+        int entier1;
+        int entier2;
+        do {
+            System.out.println("Veuillez saisir un entier non nul");
+            entier1 = scanner.nextInt();
+            System.out.println("Veuillez saisir un deuxième entier non nul");
+            entier2 = scanner.nextInt();
+        } while (entier1 == 0 || entier2 == 0) ;
+        int div = entier1/entier2;
+        System.out.println(div);
+    }
+
+    public static void regle() {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        do {
+            System.out.println("Veuillez saisir un entier positif non nul");
+            n = scanner.nextInt();
+        } while (n <= 0);
+        for (int i = 1; i <= n; i++){
+            if (i%10 == 0){
+                System.out.print("|");
+            } else {
+                System.out.print("-");
+            }
+        }
+    }
+    public static void nombrePremier(){
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        do {
+            System.out.println("Veuillez saisir un entier positif non nul");
+            n = scanner.nextInt();
+        } while (n <= 0);
+        for (int i = 2; i < n; i++){
+            if (n%i == 0){
+                System.out.println("Le nombre n'est pas un nombre premier, il est divisible par: " + i);
+                break;
+            }
+        }
+    }
+
+    public static void initialisationTableau() {
+        int[] tableau = new int[20];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < tableau.length; i++) {
+            System.out.println("Saisir un entier");
+            int entier = scanner.nextInt();
+            tableau[i] = entier;
+
+        }
+        for (int i = 0; i < tableau.length; i++){
+            System.out.print(tableau[i] + "  ");
+        }
+
+
+        int max = tableau[0];
+        int min = tableau[0];
+
+        for (int i = 0; i < tableau.length; i++){
+            if (tableau[i] > max){
+                max = tableau[i];
+            } else if (tableau[i] < min){
+                min = tableau[i];
+            }
+        }
+
+        System.out.println("Valeur maximale = " + max);
+        System.out.println("Valeur minimale = " + min);
+
+        int sum = 0;
+
+        for (int i = 0; i < tableau.length; i++){
+            if(tableau[i]%2 == 0){
+                System.out.println("Valeur paire: " + tableau[i]);
+            }
+
+            sum += tableau[i];
+        }
+
+        System.out.println("Somme des valeurs du tableau = " + sum);
+
+        for (int i = 0; i < tableau.length; i+=2){
+            System.out.println("Élement indice paire numéro "+ i + " = " + tableau[i]);
+        }
+
+        inverseTableau(tableau);
+    }
+
+    public static void inverseTableau(int[] tableau){
+        int[] tableauInverse = new int[20];
+        for (int i = 0; i < tableau.length; i++){
+            tableauInverse[i] = tableau[tableau.length - 1 - i];
+        }
+        for (int i = 0; i < tableau.length; i++){
+            System.out.print(tableau[i] + "  ");
+        }
+
+        for (int i = 0; i < tableauInverse.length; i++){
+            System.out.print(tableauInverse[i] + "  ");
+        }
+
     }
 }
-
-
